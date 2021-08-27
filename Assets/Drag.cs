b288023242727acf7b9a1be435207bd3de27097c
as <>
2021-08-27 15:01:14 +0900

    スクリプト作成、ビルド

---
+++ Assets/Drag.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Drag : MonoBehaviour
+{
+    public static event Action PuzzuleDone = delegate { };
+
+    [SerializeField]
+    private Transform standPosition;
+
+    private Vector2 initialPosition;
+
+    private Renderer rend;
+
+    private float deltaX, deltaY;
+
+    private bool moveAlowed;
+
+    private bool locked;
+    
+    void Start()
+    {
+        initialPosition = transform.position;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        moveAlowed = true;
+                        rend.sortingOrder = 3;
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+
+                    break;
+
+                case TouchPhase.Moved:
+
+                    if (moveAlowed)
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+
+                    break;
+
+                case TouchPhase.Ended:
+
+                    moveAlowed = false;
+                    rend.sortingOrder = 2;
+
+                    if (Mathf.Abs(transform.position.x - standPosition.position.x) <= 1f &&
+                        Mathf.Abs(transform.position.y - standPosition.position.y) <= 5f)
+                    {
+                        switch (PyramidControl.slotsOccupied)
+                        {
+                            case 0:
+                                transform.position = new Vector2(standPosition.position.x, -3.15f);
+                                PyramidControl.slotsOccupied = 1;
+                                break;
+
+                            case 1:
+                                transform.position = new Vector2(standPosition.position.x, -1.5f);
+                                PyramidControl.slotsOccupied = 2;
+                                break;
+
+                            case 2:
+                                transform.position = new Vector2(standPosition.position.x, 0.15f);
+                                PyramidControl.slotsOccupied = 3;
+                                break;
+
+                            case 3:
+                                transform.position = new Vector2(standPosition.position.x, 1.7f);
+                                PuzzuleDone();
+                                break;
+                        }
+
+                        locked = true;
+                    }
+                    else
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/PyramidControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class PyramidControl : MonoBehaviour
+{
+    public static int slotsOccupied;
+
+    [SerializeField]
+    private Transform[] rings;
+
+    [SerializeField]
+    private GameObject wiSign;
+
+    [SerializeField]
+    private GameObject wrongSign;
+
+
+    private void Start()
+    {
+        Drag.PuzzuleDone += CheckResults;
+        slotsOccupied = 0;
+        wiSign.SetActive(false);
+        wrongSign.SetActive(false);
+    }
+
+    public void CheckResults()
+    {
+        if (rings[0].position.y == 1.7f &&
+            rings[1].position.y == 0.15f &&
+            rings[2].position.y == -1.5f &&
+            rings[3].position.y == -3.15f)
+        {
+            wiSign.SetActive(true);
+            Invoke("ReloadGame", 2f);
+        }
+        else
+        {
+            wrongSign.SetActive(true);
+            Invoke("ReloadGame", 1f);
+        }
+    }
+
+    private void ReloadGame()
+    {
+        Drag.PuzzuleDone -= CheckResults;
+        SceneManager.LoadScene("SampleScene");
+    }
+}
